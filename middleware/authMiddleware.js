f963f67c227e2245a30f6992988298c40ef236b4
Nexusf1re <matteussantos30@hotmail.com>
2024-10-03 22:47:10 -0300

    auth fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = process.env.JWT_SECRET; 
-
+const SECRET_KEY = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];
+    
+    if (!authHeader) {
+        return res.status(401).json({ message: "Acesso negado. Token não fornecido." });
+    }
+
+    const token = authHeader.split(' ')[1]; // Extrai o token após "Bearer"
 
     if (!token) {
         return res.status(401).json({ message: "Acesso negado. Token não fornecido." });
