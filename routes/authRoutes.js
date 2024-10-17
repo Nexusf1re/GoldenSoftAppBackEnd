b4954f80b6584c4ecb5d867630bcbb233f519ba0
Matteus <matteussantos30@gmail.com>
2024-10-17 16:16:37 -0300

    1h validation token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -77,7 +77,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(
       { id: user.id, username: user.username },
       process.env.JWT_SECRET,
-      { expiresIn: '10m' }
+      { expiresIn: '1h' }
     );
 
     res.status(200).json({
